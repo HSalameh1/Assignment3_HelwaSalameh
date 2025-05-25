7d1fc6bd67b42ba820bd2658c795d602cc722fb9
Helwa <helwasalameh14@gmail.com>
2025-05-24 21:21:22 -0500

    "Add Argentavis (sky)"

---
+++ src/main/java/factory/animals/sky/Argentavis.java
@@ -0,0 +1,24 @@
+//------------------------------------------------------
+// Assignment 3
+// Written by: Helwa Salameh (1961163)
+// For SE350 Section 630 – Spring 2025
+//--------------------------------------------------------
+
+package factory.animals.sky;
+
+import factory.animals.SkyAnimal;
+
+/**
+ * Giant prehistoric bird from the Cenozoic era
+ */
+public class Argentavis implements SkyAnimal {
+    @Override
+    public String getName() {
+        return "Argentavis";
+    }
+
+    @Override
+    public String getFlying() {
+        return "soaring";
+    }
+}
