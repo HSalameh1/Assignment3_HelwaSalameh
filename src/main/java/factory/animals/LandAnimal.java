f28929738043c01cf657200c96ee7f8ecef52b51
Helwa <helwasalameh14@gmail.com>
2025-05-24 21:18:30 -0500

    "Added LandAnimal, SeaAnimal, SkyAnimal interfaces"

---
+++ src/main/java/factory/animals/LandAnimal.java
@@ -0,0 +1,12 @@
+//------------------------------------------------------
+// Assignment 3
+// Written by: Helwa Salameh (1961163)
+// For SE350 Section 630 – Spring 2025
+//--------------------------------------------------------
+
+package factory.animals;
+
+public interface LandAnimal {
+    String getName();
+    String getWalking();
+}
